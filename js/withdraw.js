1e7e0106b3e01e0694b72e02ba7e50baeba695ae
opuzayed <zayedopu4933@gmail.com>
2022-08-19 02:10:10 +0600

    error handlimg added

--- js/withdraw.js
+++ js/withdraw.js
@@ -2,21 +2,37 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
    const withdrawField = document.getElementById('withdraw-field');
    const newWithDrawAmountString = withdrawField.value;
    const newWithDrawAmount = parseFloat(newWithDrawAmountString);
+
+   withdrawField.value = "";
+
+   if(isNaN(newWithDrawAmount))
+   {
+    alert('Please type a valid amount');
+    return;
+   }
    
    const withdrawTotalElement = document.getElementById('withdraw-total');
    const previousWithdrawTotalString = withdrawTotalElement.innerText;
    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-
-   const currentWithdrawTotal = previousWithdrawTotal + newWithDrawAmount;
-   withdrawTotalElement.innerText = currentWithdrawTotal;
 
    //get balance
    const balanceTotalElement = document.getElementById('balance-total');
    const previousBalanceTotalString = balanceTotalElement.innerText;
    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
    
+
+   //check condition
+   if(newWithDrawAmount > previousBalanceTotal)
+   {
+    alert('You have a insufficient fund. Try another amount');
+    return;
+   }
+
+   const currentWithdrawTotal = previousWithdrawTotal + newWithDrawAmount;
+   withdrawTotalElement.innerText = currentWithdrawTotal;
+   
    const newBalanceTotal = previousBalanceTotal - newWithDrawAmount;
    balanceTotalElement.innerText = newBalanceTotal;
 
-   withdrawField.value = "";
+   
 })
