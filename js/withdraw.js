a8d5bfe7ca58cd9bf06ce8a23f99376717be2584
opuzayed <zayedopu4933@gmail.com>
2022-08-19 01:36:02 +0600

    withdraw calculate done

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,3 +1,15 @@
 document.getElementById('btn-withdraw').addEventListener('click', function(){
-    console.log('withdraw clicked');
+   const withdrawField = document.getElementById('withdraw-field');
+   const newWithDrawAmountString = withdrawField.value;
+   const newWithDrawAmount = parseFloat(newWithDrawAmountString);
+   
+   const withdrawTotalElement = document.getElementById('withdraw-total');
+   const previousWithdrawTotalString = withdrawTotalElement.innerText;
+   const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+   const currentWithdrawTotal = previousWithdrawTotal + newWithDrawAmount;
+   withdrawTotalElement.innerText = currentWithdrawTotal;
+
+
+   withdrawField.value = "";
 })
